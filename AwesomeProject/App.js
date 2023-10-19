5ee02cd8ea1fb0b6bb6ddfb6bb29f271e30003cd
Matheus Della <73204305+MDellaCS@users.noreply.github.com>
2023-10-19 17:48:03 +0000

    Aprendendo React Native

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,7 +1,41 @@
-export default function App() {
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+const somar = (num1, num2) => {
   return (
-    <html>
-      bom dia
-    </html>
+    <Text>Resultado de {num1} + {num2} = {num1 + num2}</Text>
   );
 }
+
+const Componente = param => {
+
+  return (
+    <View style={{margin: 15, padding: 15, borderWidth: 5, borderColor: '#e72222', backgroundColor: '#ffffbb', width: '50%' }}>
+      <Text>Oi meu nome é {param.nome}, idade {param.idade}!</Text>
+      <TextInput placeholder='Input' />
+      <Text>{somar(2, 2)}</Text>
+    </View>
+  );
+
+};
+
+const Componente2 = () => {
+
+  return (
+    <View style={{ backgroundColor: '#bbbbcc', width: '50%' }}>
+      <Text>Componente Maior</Text>
+      <Componente nome="A" idade="21" />
+      <Componente nome="B" idade="15" />
+      <Componente nome="C" />
+      <Componente nome="D" idade="5" />
+      <Componente nome="E" />
+      <Componente idade="24" />
+      <Componente />
+      <Componente />
+      <Componente />
+    </View>
+  );
+
+};
+
+export default Componente2;
