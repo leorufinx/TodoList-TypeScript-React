74253b7286ac207b310230abad14790defa17b7d
Matheus Della <73204305+MDellaCS@users.noreply.github.com>
2023-10-19 19:47:24 +0000

    Android SDK

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View, Button } from 'react-native';
 
 const somar = (num1, num2) => {
   return (
@@ -9,10 +9,21 @@ const somar = (num1, num2) => {
 
 const Componente = param => {
 
+  const [genero, setGenero] = useState(true);
+
   return (
-    <View style={{margin: 15, padding: 15, borderWidth: 5, borderColor: '#e72222', backgroundColor: '#ffffbb', width: '50%' }}>
+    <View style={{ margin: 15, padding: 15, borderWidth: 5, borderColor: '#e72222', backgroundColor: '#ffffbb', width: '50%' }}>
+
+      <Text style={{ fontFamily: 'Comic Sans MS', fontSize: 25 }}>Componente Child</Text>
+
       <Text>Oi meu nome é {param.nome}, idade {param.idade}!</Text>
-      <TextInput placeholder='Input' />
+
+      <Text>Eu sou {genero ? 'homem' : 'mulher'}</Text>
+
+      <Button onPress={() => { setGenero(!genero); }} title={genero ? 'Quero ser Mulher' : 'Quero ser Homem'} />
+
+      <TextInput style={{ margin: 5, padding: 5, backgroundColor: '#ff5555' }} placeholder='Digite algo' />
+
       <Text>{somar(2, 2)}</Text>
     </View>
   );
@@ -22,17 +33,14 @@ const Componente = param => {
 const Componente2 = () => {
 
   return (
-    <View style={{ backgroundColor: '#bbbbcc', width: '50%' }}>
-      <Text>Componente Maior</Text>
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#bbbbcc', width: '100%' }}>
+      <Text style={{ fontSize: 35 }}>Componente Parent</Text>
       <Componente nome="A" idade="21" />
       <Componente nome="B" idade="15" />
       <Componente nome="C" />
       <Componente nome="D" idade="5" />
       <Componente nome="E" />
       <Componente idade="24" />
-      <Componente />
-      <Componente />
-      <Componente />
     </View>
   );
 
