a38fd62d3cf2b7c1647cfc0c965b19bdc70b636d
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-29 23:30:02 -0300

    Novos estilos e sass

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.scss";
 import Home from "./pages/Home";
 
 function App() {
--- todo-list/src/components/Form.jsx
+++ todo-list/src/components/Form.jsx
@@ -2,30 +2,34 @@ import { Button, Paper, TextField } from "@mui/material";
 import React, { useState } from "react";
 
 export default function Form({ addTodo }) {
-  const [text, setText] = useState(null);
-  const [id, setId] = useState(0);
+   const [text, setText] = useState(null);
+   const [id, setId] = useState(0);
 
-  const todoCreate = (text) => {
-    const todoObj = { text: text, id: id };
-    setId(id + 1);
-    addTodo(todoObj);
-    document.getElementById("outlined-basic").value = null;
-  };
+   const todoCreate = (text) => {
+      const todoObj = { text: text, id: id };
+      setId(id + 1);
+      addTodo(todoObj);
+      document.getElementById("outlined-basic").value = null;
+   };
 
-  return (
-    <Paper style={{ padding: "1em" }}>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <TextField
-          id="outlined-basic"
-          label="Tarefa"
-          variant="outlined"
-          onChange={(e) => setText(e.target.value)}
-          fullWidth
-        />
-        <Button variant="text" onClick={() => todoCreate(text)}>
-          Add
-        </Button>
+   return (
+      <div className="form">
+         <div className="h1">Tarefa</div>
+         <div style={{ display: "inline-block", justifyContent: "center" }}>
+
+            <label htmlFor="titulo" className="lbl">Título</label>
+            <input id="titulo" type="text" className="formInput" />
+
+            <label htmlFor="descricao" className="lbl">Descrição</label>
+            <input id="descricao" type="text" className="formInput" />
+
+            <label htmlFor="texto" className="lbl">Texto</label>
+            <input id="texto" type="text" className="formInput" />
+
+            <Button variant="text" onClick={() => todoCreate(text)}>
+               Criar
+            </Button>
+         </div>
       </div>
-    </Paper>
-  );
+   );
 }
