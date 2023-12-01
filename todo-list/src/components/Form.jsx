33373a3e0ffc5a15f2fd87508560b02e2ecf9936
Matheus Della <matheusdellacrocce@hotmail.com>
2023-11-30 23:08:24 -0300

    Novo front

--- todo-list/src/components/Form.jsx
+++ todo-list/src/components/Form.jsx
@@ -1,35 +1,38 @@
 import { Button, Paper, TextField } from "@mui/material";
 import React, { useState } from "react";
+import { InputField, InputArea } from "../components/Input";
 
 export default function Form({ addTodo }) {
-   const [text, setText] = useState(null);
-   const [id, setId] = useState(0);
+   const [getTitulo, setTitulo] = useState('');
+   const [getDescricao, setDescricao] = useState('');
+   const [getTexto, setTexto] = useState('');
 
-   const todoCreate = (text) => {
-      const todoObj = { text: text, id: id };
-      setId(id + 1);
-      addTodo(todoObj);
-      document.getElementById("outlined-basic").value = null;
-   };
+   // const todoCreate = (text) => {
+   //    const todoObj = { text: text, id: id };
+   //    setId(id + 1);
+   //    addTodo(todoObj);
+   //    document.getElementById("outlined-basic").value = null;
+   // };
 
    return (
+
       <div className="form">
          <div className="h1">Tarefa</div>
-         <div style={{ display: "inline-block", justifyContent: "center" }}>
 
-            <label htmlFor="titulo" className="lbl">Título</label>
-            <input id="titulo" type="text" className="formInput" />
-
-            <label htmlFor="descricao" className="lbl">Descrição</label>
-            <input id="descricao" type="text" className="formInput" />
-
-            <label htmlFor="texto" className="lbl">Texto</label>
-            <input id="texto" type="text" className="formInput" />
-
-            <Button variant="text" onClick={() => todoCreate(text)}>
-               Criar
-            </Button>
+         <div className="row">
+            <InputField id="titulo" title="Título" type="text" value={getTitulo} onChange={setTitulo} />
          </div>
+
+         <div className="row">
+            <InputArea id="descricao" title="Descrição" type="text" />
+         </div>
+
+         <input type="button" className="btn" value="Criar" />
+
+         {/* <Button variant="text" onClick={() => todoCreate(text)}>
+            Criar
+         </Button> */}
       </div>
+
    );
 }
