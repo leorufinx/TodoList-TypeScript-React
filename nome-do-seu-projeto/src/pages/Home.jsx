980c6a9401036103d518d5ee2cbf9928266d4cce
Matheus Della <matheusdellacrocce@hotmail.com>
2023-12-04 01:04:21 -0300

    commit

---
+++ nome-do-seu-projeto/src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { InputField, InputArea } from "../components/Input";
+
+export default function Home() {
+   const [getTitulo, setTitulo] = useState("");
+   const [getDescricao, setDescricao] = useState("");
+
+   const todoCreate = () => {
+
+      console.log(getTitulo + "\n" + getDescricao);
+
+      setTitulo("");
+      setDescricao("");
+   };
+
+   const logout = () => {
+      localStorage.setItem("authenticated", "false");
+      localStorage.setItem("name", null);
+      localStorage.setItem("password", null);
+   };
+
+   return (
+      <>
+         <input type="button" className="btn" value="Sair" onClick={() => logout} />
+
+         <div className="form">
+
+            <div className="h1">Tarefa</div>
+
+            <div className="row">
+               <InputField id="titulo" title="Título" type="text" value={getTitulo} onChange={setTitulo} />
+            </div>
+
+            <div className="row">
+               <InputArea id="descricao" title="Descrição" type="text" value={getDescricao} onChange={setDescricao} />
+            </div>
+
+            <input type="button" className="btn" value="Criar" onClick={todoCreate} />
+
+         </div>
+      </>
+   );
+}
